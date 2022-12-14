593c45fb44f53f8ac474edd82417f198fd15d34e
thumbthing <ilscc0902@gmail.com>
2022-12-14 11:28:58 +0900

    return to proto type

--- productmanager-master/src/main/java/productmanager/client/ProductClient.java
+++ productmanager-master/src/main/java/productmanager/client/ProductClient.java
@@ -12,58 +12,46 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ProductClient {
+    // 필드를 작성하고 필드를 일단은 초기화
     private Socket socket;
+
+    // input 에 넣어서 보낸다
     private DataInputStream dis;
+
+    // output 에 담겨진 걸 읽는다
     private DataOutputStream dos;
     private Scanner scanner;
 
+
     public static void main(String[] args) {
-        ProductClient productClient = new ProductClient();
+        // 소켓 만들어준다
         try {
+            ProductClient productClient = new ProductClient();
             productClient.start();
-        } catch (IOException e) {}
+        } catch (Exception e) {}
+
     }
 
     private void start() throws IOException {
-        // 서버 연결 필요
-        socket = new Socket("localhost", 50001);
+        // 서버 연결 192.168.10.248
+        socket = new Socket("localhost",50003);
         dis = new DataInputStream(socket.getInputStream());
         dos = new DataOutputStream(socket.getOutputStream());
-        System.out.println("[클라이언트] 서버에 연결됨");
+        System.out.println("클라이언트 서버에 연결됨");
 
         scanner = new Scanner(System.in);
-
         list();
-        receive();
-
-        while(true) {
+        while (true) {
             showMenu();
         }
     }
 
-    private void receive() {
-        Thread thread = new Thread(()-> {
-            while (true) {
-                JSONObject response = null;
-                try {
-                    response = new JSONObject(dis.readUTF());
-                    if(response.getString("status").equals("updated")) {
-                        list();
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        thread.start();
-    }
-
     private void showMenu() throws IOException {
         System.out.println();
-        System.out.println("---------------------------------------------------------------");
-        System.out.println("메뉴: 1.Create | 2.Update | 3.Delete | 4.Exit");
+        System.out.println("-----------------------------------------------");
+        System.out.println("메뉴 1: Create | 2: Update | 3: Delete | 4: Exit");
         System.out.print("선택: ");
-        int menuNo = Integer.parseInt(scanner.nextLine());
+        Integer menuNo = scanner.nextInt();
         System.out.println();
 
         switch (menuNo) {
@@ -81,122 +69,177 @@ public class ProductClient {
                 break;
         }
     }
-
     private void create() throws IOException {
-        // 1. scanner로 입력받아서 상품(Product) 생성
-        // 2. Json으로 변환해서 서버에 요청
-        System.out.println("[상품 생성]");
+        //상품 생성
+        // scanner로 입력 받음
+        // JSON으로 변환해서 서버에 요청
+        System.out.println("상품 생성");
         Product product = new Product();
-        System.out.print("상품 이름: ");
-        product.setName(scanner.nextLine());
-        System.out.print("상품 가격: ");
-        product.setPrice(Integer.parseInt(scanner.nextLine()));
-        System.out.print("상품 재고: ");
-        product.setStock(Integer.parseInt(scanner.nextLine()));
+        System.out.println("상품 이름");
+        product.setName(scanner.next());
+        System.out.println("상품 가격");
+        product.setPrice(scanner.nextInt());
+        System.out.println("상품 제고");
+        product.setStock(scanner.nextInt());
 
+
+        // json 파일을 생성하고 거기에 데이터를 넣는다
         JSONObject data = new JSONObject();
         data.put("name", product.getName());
         data.put("price", product.getPrice());
         data.put("stock", product.getStock());
 
+
+        // json 파일을 하나 더 생성 해서
+        // 위에 생성한 json 파일과 requestcode를 넣는다
         JSONObject request = new JSONObject();
         request.put("menu", RequestCode.CREATE);
         request.put("data", data);
 
+        // 생성한 json 파일을 직력화하고 DataOutputStream에 넣고 flush
         dos.writeUTF(request.toString());
         dos.flush();
 
+        // 서버로 부터 받은 직렬화된 json 파일을 JSONObject 형식으로 바꿔서 읽는다
+        JSONObject response = new JSONObject(dis.readUTF());
+        if(response.getString("status").equals("success")) {
+            list();
+        }
     }
-    private void update() throws IOException {
-        // 콘솔에서 변경할 상품 정보 입력받기
-        System.out.println("[상품 수정]");
-        Product product = new Product();
-        System.out.print("상품 번호: ");
-        product.setNo(Integer.parseInt(scanner.nextLine()));
-        System.out.print("이름 변경: ");
-        product.setName(scanner.nextLine());
-        System.out.print("가격 변경: ");
-        product.setPrice(Integer.parseInt(scanner.nextLine()));
-        System.out.print("재고 변경: ");
-        product.setStock(Integer.parseInt(scanner.nextLine()));
 
-        // 상품정보를 JSON으로 변환
+    private void update() throws IOException {
+        // Product 가 맵 형태이다
+        System.out.println("상품 수정");
+        Product product = new Product();
+        System.out.println("상품 번호");
+        product.setNo(scanner.nextInt());
+        System.out.println("상품 이름");
+        product.setName(scanner.next());
+        System.out.println("상품 가격");
+        product.setPrice(scanner.nextInt());
+        System.out.println("상품 제고");
+        product.setStock(scanner.nextInt());
+
+
+        // data 라는 JSON 형식의 파일을 생성하고
+        // product에 입력된 데이터를 키값과 함께 넣어준다
         JSONObject data = new JSONObject();
         data.put("no", product.getNo());
         data.put("name", product.getName());
         data.put("price", product.getPrice());
         data.put("stock", product.getStock());
+
+
+        // 위에 생성한 JSON 파일과 RequestCode 를
+        // 다시 JSON 형식으로 바꾸고
         JSONObject request = new JSONObject();
         request.put("menu", RequestCode.UPDATE);
         request.put("data", data);
 
-        // JSON 파일을 직렬화해서 서버로 request
+        // DataOutPutStream에 직렬화한 JSON을 넣고 FLUSH
         dos.writeUTF(request.toString());
         dos.flush();
+        // 응답 받은거 확인
+        JSONObject response = new JSONObject(dis.readUTF());
+        if(response.getString("status").equals("success")) {
+            list();
+        }
     }
-    private void delete() throws IOException {
-        // 삭제할 상품번호 입력받기
-        System.out.println("[상품 삭제]");
-        System.out.print("상품 번호: ");
-        int no = Integer.parseInt(scanner.nextLine());
 
+    private void delete() throws IOException {
+        //상품 삭제
+        // 상품 번호로
+        // 상품 번호 입력 받기
+        // 상품 삭제 요청 (서버에게)
+        System.out.println("상품 삭제");
+        System.out.print("상품 번호: ");
+        int no = scanner.nextInt();
+
+
+        // 어떤 상품을 삭제할지 정해야 하기 때문에
+        // 상품 번호를 입력하고 그걸 JSON 형식으로 만들어준다
         JSONObject data = new JSONObject();
         data.put("no", no);
 
-        // 상품 삭제 요청 (-> 서버)
+        // 삭제 요청 (서버로)
+        // 요청 코드와 상품 번호를 JSON에 넣어준다
         JSONObject request = new JSONObject();
         request.put("menu", RequestCode.DELETE);
         request.put("data", data);
 
+        // 위에 만들어준 JSON 파일을 직렬화 해서
+        // 서버로 보내주는데 그때
+        // DataOutPutStream을 사용해서 보내주고 FLUSH
         dos.writeUTF(request.toString());
         dos.flush();
-
+        // 응답 받은거 확인
+        // 서버로 부터 받은 JSON 파일을
+        // DataInputStream에서 꺼내서 읽어오고
+        // 그걸 JSONObject 형식으로 변환해서
+        // key 값으로 값을 불러와서 함수를 돌려준다
+        JSONObject response = new JSONObject(dis.readUTF());
+        if(response.getString("status").equals("success")) {
+            list();
+        }
     }
     private void exit() {
+//        socket.close();
+//        JSONObject data = new JSONObject();
+//        data.put("Socket", socket);
+//
+//        JSONObject request = new JSONObject();
+//        request.put("menu", RequestCode.EXIT);
+//        request.put("data" ,data);
+//
+//        dos.writeUTF(request.toString());
+//        dos.flush();
         stop();
     }
 
     private void stop() {
         try {
+            // 서버 연결을 끊고 싶을때
+            // 클라이언트 측에서 일단 소켓을 닫아주면 된다
+
             socket.close();
             scanner.close();
-        } catch (Exception e) {}
-        System.out.println("[클라이언트] 종료됨");
+        } catch (Exception e) {
+            System.out.println("[클라이언트] 종료됨");
+        }
     }
 
-    // 클라이언트가 서버한테 상품목록좀 보여달라고 요청
-    private void list() throws IOException {
-        // 타이틀 및 컬럼명 출력
-        System.out.println();
-        System.out.println("[상품 목록]");
-        System.out.println("---------------------------------------------------------------");
-        System.out.printf("%-6s%-30s%-15s%-10s\n", "no", "name", "price", "stock");
-        System.out.println("---------------------------------------------------------------");
 
-        // 상품 목록 요청
-        // JSON 만들어서 서버에 요청
-        // GET 메소드와 같은 동작
+    // 클라이언트가 서버에게
+    // 상품 목록을 보여달라고 하는 메소드
+    private void list() throws IOException {
+        System.out.println("-----------------------------------------------");
+        System.out.println("no  | name           | price        | stock    ");
+        System.out.println("-----------------------------------------------");
+        System.out.printf("%-6s%-30s%-15s%-10s\n", "no", "name", "price", "stock");
+
+
+        //상품 목록 요청
+        //JSON 만들어서 서버에 요청 하는 것
+        // get 메소드 와 같은 동작
+        //
         JSONObject request = new JSONObject();
         request.put("menu", RequestCode.READ);
-//        request.put("data", new JSONObject());
+        request.put("data", "");
         dos.writeUTF(request.toString());
         dos.flush();
 
-        // response
+        // 응답을 받아야함
         JSONObject response = new JSONObject(dis.readUTF());
         if(response.getString("status").equals("success")) {
             JSONArray data = response.getJSONArray("data");
-            for(int i=0; i<data.length(); i++) {
+            for (int i = 0; i < data.length(); i++) {
                 JSONObject product = data.getJSONObject(i);
-                System.out.printf(
-                        "%-6d%-30s%-15d%-10d\n",
+                System.out.printf("%-6s%-30s%-15s%-10s\n",
                         product.getInt("no"),
                         product.getString("name"),
                         product.getInt("price"),
-                        product.getInt("stock")
-                );
+                        product.getInt("stock"));
             }
         }
-
     }
 }
--- productmanager-master/src/main/java/productmanager/server/ProductServer.java
+++ productmanager-master/src/main/java/productmanager/server/ProductServer.java
@@ -15,73 +15,53 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ProductServer {
-    // 필드
+    // 서버에서 뭐가 필요할까 (소켓,
     private ServerSocket serverSocket;
-    private ExecutorService threadPool;
+    private ExecutorService threadPool = Executors.newFixedThreadPool(100);
+
+    // 소켓 관리
+
+
     private List<Product> products;
-
-    Set<SocketClient> clients = Collections.synchronizedSet(new HashSet<>());
-
-    private int sequence = 0;
+    // products는 db라고 생각해보자
+    private int sequence;
 
     public static void main(String[] args) {
-        // 서버 객체 생성
         ProductServer productServer = new ProductServer();
-
         try {
             productServer.start();
         } catch (IOException e) {
             System.out.println(e.getMessage());
-//            productServer.stop();
+            // ProductServer.stop();
         }
     }
 
-    private void start() throws IOException {
-        // 소켓에 포트 바인딩 (전화기에 번호부여)
-        serverSocket = new ServerSocket(50001);
-        threadPool = Executors.newFixedThreadPool(100);
-        products = new Vector<>();
+    public void start() throws IOException {
+        // 소켓 포트 바인딩 해줘야함
+        // 새로운 연결을 대기하고 있다 (연결이 되면 새로운 소켓을 생성한다)
+        // 클라이언트와 새로운 소켓의 통신이 시작된다
 
-        // test용 dummy 데이터
+        // socket client 가 이 기능을 담당함
+        serverSocket = new ServerSocket(50003);
+
+        threadPool = Executors.newFixedThreadPool(100);
+        // 락을 걸기 위해서 벡터를 쓴다
+        products = new Vector<Product>();
+
         products.add(
-                new Product(sequence++, "삼다수", 1000, 20)
+                new Product(sequence++ , "삼다수", 1000, 20)
         );
 
-        System.out.println("[서버] 시작됨");
 
         while (true) {
-            // 이제 해당 클라이언트와 리턴된 소켓을 통해 서버와 TCP 통신을 할 수 있다.
+            // 소켓을 생성하고 활용해야함
+            // 이제 accept 받은 리턴된 소켓을 통해 통신을 할 수 있다
             Socket socket = serverSocket.accept();
-            SocketClient sc = new SocketClient(socket);
+            SocketClient sc  = new SocketClient(socket);
         }
     }
 
-    private void notifyUpdatedToClients() throws IOException {
-        for (SocketClient client: clients) {
-            // response 보내기
-            // 1. JSON만들기
-            JSONObject response = new JSONObject();
-            response.put("status", "updated");
-            response.put("data", "");
-            // 2. 직렬화하기(문자열화)
-            client.dos.writeUTF(response.toString());
-            client.dos.flush();
-        }
-    }
 
-    private void addSocketClient(SocketClient socketClient) {
-        clients.add(socketClient);
-        System.out.println("새로운 클라이언트 접속");
-        System.out.println("현재 클라이언트 수: " + clients.size() + "\n");
-    }
-
-    private void removeSocketClient(SocketClient socketClient) {
-        clients.remove(socketClient);
-        System.out.println("클라이언트 나감");
-        System.out.println("현재 클라이언트 수: " + clients.size() + "\n");
-    }
-
-    // 중첩 클래스
     public class SocketClient {
         private Socket socket;
         // 해당 클라이언트로부터 요청을 받을 때 사용
@@ -90,29 +70,30 @@ public class ProductServer {
         private DataOutputStream dos;
 
         public SocketClient(Socket socket) {
+
             try {
                 this.socket = socket;
+                // 데이터 받은 걸 읽을때 dis 를 보면 된다
                 this.dis = new DataInputStream(socket.getInputStream());
                 this.dos = new DataOutputStream(socket.getOutputStream());
-                addSocketClient(this);
                 receive();
-            } catch (IOException e) {}
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
         }
-
-        // 요청 (클라이언트 -> 서버)
-            // 데이터(JSON)를 읽고 어떤 요청인지 확인
-            // dis을 통해 데이터를 읽음
-        // String -> 객체화 시켜서 사용
-        // {
-        //      menu: 메뉴 번호,
-        //      data: { … }
-        // }
+        // 서버가 사용하는 소켓을 어떻게 쓸 것인가
+        // 데이터 요청 (clinet -> server)
+        // 데이터를 읽고
+        // 어떤 요청인지 확인
+        // dis를 통해 데이터를 읽으면 된다
         public void receive() {
+            //execute 안에 처리할 작업을 써준다
             threadPool.execute(()->{
                 try {
-                    while(true) {
+                    while (true) {
                         String receiveJson = dis.readUTF();
 
+                        //String -> Object 시켜서 사용
                         JSONObject request = new JSONObject(receiveJson);
                         int menu = request.getInt("menu");
 
@@ -120,19 +101,22 @@ public class ProductServer {
                             case RequestCode.READ:
                                 list();
                                 break;
-                            case RequestCode.CREATE:
-                                create(request);
+                            case RequestCode.DELETE:
+                                delete(request);
                                 break;
                             case RequestCode.UPDATE:
                                 update(request);
                                 break;
-                            case RequestCode.DELETE:
-                                delete(request);
+                            case RequestCode.CREATE:
+                                create(request);
                                 break;
+                            case RequestCode.EXIT:
+                                exit(request);
+                                break;
+
                         }
                     }
                 } catch (IOException e) {
-                    removeSocketClient(this);
                     close();
                 }
             });
@@ -141,16 +125,20 @@ public class ProductServer {
         private void close() {
             try {
                 socket.close();
-            } catch (IOException e) {}
+            }catch (Exception e) {
+                System.out.println("[클라이언트] 접속 끊음");
+            }
         }
-
         // list
-        // HTTP의 GET 메소드랑 비슷한 동작
-        // 상품 목록을 보여줌 (서버 -> 클라이언트)
+        // 상품 목록을 클라이언트에게 보여준다
+        // 서버야 너가 가지고 있는 목록 좀 보여줘
         public void list() throws IOException {
+
             JSONArray data = new JSONArray();
-            // Product 객체 -> JSON 타입으로 변환
-            for (Product p : products) {
+            // product 객체를 가져와서
+            // JSON 타입으로 바꿔줘야함
+
+            for(Product p : products) {
                 JSONObject product = new JSONObject();
                 product.put("no", p.getNo());
                 product.put("name", p.getName());
@@ -164,45 +152,62 @@ public class ProductServer {
             dos.writeUTF(response.toString());
             dos.flush();
         }
-        // create
-        // 클라이언트가 상품을 만들어달라고 요청
-        // 만든 상품은 서버에 저장됨
+        // Create
         public void create(JSONObject request) throws IOException {
-            // data 안에 상품이름, 가격, 재고수량
             JSONObject data = request.getJSONObject("data");
+            // 클라이언트에서 요청이 서버로 들어옴
             Product product = new Product();
             product.setNo(sequence++);
             product.setName(data.getString("name"));
             product.setPrice(data.getInt("price"));
             product.setStock(data.getInt("stock"));
-            
+
             products.add(product);
 
-            notifyUpdatedToClients();
+            //response 보내기
+            // 1. JSON 만들기
+            JSONObject response = new JSONObject();
+            response.put("status", "success");
+            response.put("data", "");
+            // 2. 직렬화 하기
+            dos.writeUTF(response.toString());
+            dos.flush();
+
+
         }
-        // update
+
+        // 여기 해야함
+        // Update
         public void update(JSONObject request) throws IOException {
-            // 받은 JSON파일을 파싱해서 읽기
+            // 데이터를 읽고
+            // 여기서는 requst 는 한번 펼쳐 졌다
             JSONObject data = request.getJSONObject("data");
-            // 해당하는 상품정보 업데이트
             int no = data.getInt("no");
-            for(int i = 0; i < products.size(); i++) {
-                Product product = products.get(i);
-                if(product.getNo() == no) {
+
+            Iterator<Product> iterator = products.iterator();
+            while(iterator.hasNext()) {
+                Product product = iterator.next();
+                if (product.getNo() == no) {
                     product.setName(data.getString("name"));
                     product.setPrice(data.getInt("price"));
                     product.setStock(data.getInt("stock"));
                 }
             }
-            // 클라이언트한테 response
-            notifyUpdatedToClients();
+            // reponse
+            JSONObject response = new JSONObject();
+            response.put("status", "success");
+            response.put("data", new JSONObject());
+            dos.writeUTF(response.toString());
+            dos.flush();
+
         }
-        // delete
-        public void delete(JSONObject request) throws IOException {
-            // 데이터 읽고
+        // Delete
+        public  void delete(JSONObject request) throws IOException {
+            // 데이터를 읽고
+            // 여기서는 requst 는 한번 펼쳐 졌다
             JSONObject data = request.getJSONObject("data");
             int no = data.getInt("no");
-            // 해당 no 이 products 안에 있는지 확인하고 삭제
+            // 해당 no 가 products에 있는 지 확인 후 삭제
             Iterator<Product> iterator = products.iterator();
             while(iterator.hasNext()) {
                 Product product = iterator.next();
@@ -210,9 +215,21 @@ public class ProductServer {
                     iterator.remove();
                 }
             }
+            // reponse
+            JSONObject response = new JSONObject();
+            response.put("status", "success");
+            response.put("data", new JSONObject());
+            dos.writeUTF(response.toString());
+            dos.flush();
+        }
 
-            // response
-            notifyUpdatedToClients();
+        public void exit(JSONObject request) throws IOException {
+            JSONObject data = request.getJSONObject("data");
+
+            System.out.println("user out");
+
+
+
         }
     }
 }
